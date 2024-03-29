937561193a547453628a20cab2f9006350710037
akshayrajeevnambiar <akshayrajeevnambiar@gmail.com>
2024-03-28 23:13:51 -0400

    initial commit

---
+++ components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className="footer-container">
+      <p>2023 ARN Headphones All rights reserverd</p>
+      <p className="icons">
+      </p>
+    </div>
+  )
+}
+
+export default Footer
